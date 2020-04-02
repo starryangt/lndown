c19d8bad4ecef6e334671fbf96ac7425341336e1
Starr Yang <starryangt@gmail.com>
2020-04-02 18:02:21 -0400

    Updated client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <style>
@@ -17,16 +15,20 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,103 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-col fluid>
+        <v-row>
+          <v-text-field
+            label="Title"
+            v-model="title"
+          ></v-text-field>
+        </v-row>
+        <v-row>
+          <v-text-field
+            label="Author"
+            v-model="author"
+          ></v-text-field>
+        </v-row>
+        <v-row>
+          <v-select
+          :items="scraper_items"
+          label="Scraper"
+          v-model="scraper"
+          ></v-select>
+        </v-row>
+        <v-row>
+          <v-select
+          :items="parser_items"
+          label="Parser"
+          v-model="parser"
+          ></v-select>
+        </v-row>
+      </v-col>
+      <v-col fluid>
+        <v-row fluid>
+          <p>Paste URLs Here</p>
+          <v-card
+          outlined
+          class="mx-auto"
+          min-width="100%"
+          min-height="200px"
+          max-height="200px"
+          style="overflow: scroll"
+          >
+            <draggable v-model="urls" group="people" @start="drag=true" @end="drag=false">
+                <div v-for="element in urls" :key="element.title">{{element.title}}</div>
+            </draggable>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import draggable from 'vuedraggable'
+
+//import { eel } from './eel.js'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    draggable
+  },
+  data: () => ({
+    title: '',
+    author: '',
+    cover: '',
+    scraper: 'http',
+    parser: 'readability',
+    urls: [],
+    scraper_items: [
+      'http'
+    ],
+    parser_items: [
+      'readability',
+      'dragnet'
+    ]
+  }),
+  methods: {
+    handle_paste: function(e){
+      e.stopPropagation()
+      e.preventDefault()
+
+      let clipboardData = e.clipboardData || window.clipboardData
+      let pastedData = clipboardData.getData('text/html')
+
+      let doc = new DOMParser().parseFromString(pastedData, 'text/html')
+      let links = doc.querySelectorAll("a")
+
+      for(let a of links){
+        this.urls.push({
+          'title': a.textContent,
+          'href': a.getAttribute("href")
+        })
+      }
+    }
+  },
+  mounted: function(){
+    document.body.addEventListener('paste', this.handle_paste)
   }
 }
 </script>
